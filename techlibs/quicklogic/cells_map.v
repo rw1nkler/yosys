ff8da072b6260bcc67808fb452529684fbe9ae69
Maciej Kurc <mkurc@antmicro.com>
2020-07-28 09:33:54 +0200

    Added techmaps for DFFE, split LUT and FF techmaps into separate files.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- techlibs/quicklogic/cells_map.v
+++ techlibs/quicklogic/cells_map.v
@@ -1,102 +1,28 @@
-module \$_DLATCH_P_ (E, D, Q);
-    input E;
-    input D;
-    output reg Q;
-    LUT3 #(.INIT(8'b11100010)) latchimpl (.O(Q), .I2(D), .I1(E), .I0(Q));
+module \$_MUX8_ (A, B, C, D, E, F, G, H, S, T, U, Y);
+    input A, B, C, D, E, F, G, H, S, T, U;
+    output Y;
+    mux8x0 _TECHMAP_REPLACE_ (.A(A), .B(B), .C(C), .D(D), .E(E), .F(F), .G(G), .H(H), .S0(S), .S1(T), .S2(U), .Q(Y));
 endmodule
 
-module \$_DLATCH_N_ (E, D, Q);
-    input E;
-    input D;
-    output reg Q;
-    LUT3 #(.INIT(8'b10111000)) latchimpl (.O(Q), .I2(D), .I1(E), .I0(Q));
+module \$_MUX4_ (A, B, C, D, S, T, U, Y);
+    input A, B, C, D, S, T, U;
+    output Y;
+    mux4x0 _TECHMAP_REPLACE_ (.A(A), .B(B), .C(C), .D(D), .S0(S), .S1(T), .Q(Y));
 endmodule
 
-module \$_DLATCHSR_NNN_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b101011001111111)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b1000)) sedr (.O(Q), .I1(R), .I0(SEDQ));
+module \$_MUX_ (A, B, S, Y);
+    input A, B, S;
+    output Y;
+    mux2x0 _TECHMAP_REPLACE_ (.A(A), .B(B), .S(S), .Q(Y));
 endmodule
 
-module \$_DLATCHSR_NNP_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b101011001111111)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b0010)) sedr (.O(Q), .I1(R), .I0(SEDQ));
-endmodule
+module \$_NOT_ (A, Y);
+    input  A;
+    output Y;
 
-module \$_DLATCHSR_NPN_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b111111111010110)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b1000)) sedr (.O(Q), .I1(R), .I0(SEDQ));
-endmodule
-
-module \$_DLATCHSR_NPP_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b111111111010110)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b0010)) sedr (.O(Q), .I1(R), .I0(SEDQ));
-endmodule
-
-module \$_DLATCHSR_PNN_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b110010101111111)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b1000)) sedr (.O(Q), .I1(R), .I0(SEDQ));
-endmodule
-
-module \$_DLATCHSR_PNP_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b110010101111111)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b0010)) sedr (.O(Q), .I1(R), .I0(SEDQ));
-endmodule
-
-module \$_DLATCHSR_PPN_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b111111111100101)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b1000)) sedr (.O(Q), .I1(R), .I0(SEDQ));
-endmodule
-
-module \$_DLATCHSR_PPP_ (E, S, R, D, Q);
-    input E;
-    input S;
-    input R;
-    input D;
-    output Q;
-    wire SEDQ;
-    LUT4 #(.INIT(16'b11111111110010)) sedqlut (.O(SEDQ), .I3(S), .I2(E), .I1(D), .I0(Q));
-    LUT2 #(.INIT(4'b0010)) sedr (.O(Q), .I1(R), .I0(SEDQ));
+    inv _TECHMAP_REPLACE_ (
+        .A(A),
+        .Q(Y)
+    );    
 endmodule
 
