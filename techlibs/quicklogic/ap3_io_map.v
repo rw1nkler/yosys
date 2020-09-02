ceb445cac4bdd9648358095085148e742114f8d4
Lalit Sharma <lsharma@quicklogic.com>
2020-09-02 11:21:02 +0530

    Adding io_map file for AP3
    Signed-off-by: Lalit Sharma <lsharma@quicklogic.com>

---
+++ techlibs/quicklogic/ap3_io_map.v
@@ -0,0 +1,22 @@
+module \$__out_buff (Q, A);
+	output Q;
+    input A;
+
+    parameter _TECHMAP_CONSTMSK_A_ = 0;
+    parameter _TECHMAP_CONSTVAL_A_ = 0;
+
+    if(_TECHMAP_CONSTMSK_A_ == 1) begin
+        d_buff _TECHMAP_REPLACE_ (.Q(Q), .EN(_TECHMAP_CONSTVAL_A_));
+    end
+    else begin
+        out_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
+    end
+endmodule
+
+module \$__in_buff (Q, A);
+	output Q;
+    input A;
+
+    in_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
+
+endmodule
