ff5ebec1a60172396e23f947825af2a585c26173
Lalit Sharma <lsharma@quicklogic.com>
2020-09-04 14:38:17 +0530

    Updating in_reg/out_reg def in AP3. Replacing in_reg, out_reg with io_reg in AP2
    Signed-off-by: Lalit Sharma <lsharma@quicklogic.com>

--- techlibs/quicklogic/ap2_io_map.v
+++ techlibs/quicklogic/ap2_io_map.v
@@ -6,7 +6,7 @@ module \$__out_buff (Q, A);
     parameter _TECHMAP_CONSTVAL_A_ = 0;
 
     if(_TECHMAP_CONSTMSK_A_ == 1) begin
-        d_buff _TECHMAP_REPLACE_ (.Q(Q), .EN(_TECHMAP_CONSTVAL_A_));
+        d_buff _TECHMAP_REPLACE_ (.OUT_DBUF(Q), .IN_DBUF(_TECHMAP_CONSTVAL_A_));
     end
     else begin
         out_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
--- techlibs/quicklogic/ap3_io_map.v
+++ techlibs/quicklogic/ap3_io_map.v
@@ -6,7 +6,7 @@ module \$__out_buff (Q, A);
     parameter _TECHMAP_CONSTVAL_A_ = 0;
 
     if(_TECHMAP_CONSTMSK_A_ == 1) begin
-        d_buff _TECHMAP_REPLACE_ (.Q(Q), .EN(_TECHMAP_CONSTVAL_A_));
+        d_buff #(.DSEL(_TECHMAP_CONSTVAL_A_)) _TECHMAP_REPLACE_ (.Q(Q));
     end
     else begin
         out_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
