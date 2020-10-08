d0ef82714158b17daac57867175479292a566bcf
Lalit Sharma <lsharma@quicklogic.com>
2020-10-08 17:24:56 +0530

    Updating IO names to lower case
    Signed-off-by: Lalit Sharma <lsharma@quicklogic.com>

--- techlibs/quicklogic/ap3_io_map.v
+++ techlibs/quicklogic/ap3_io_map.v
@@ -6,10 +6,10 @@ module \$__out_buff (Q, A);
     parameter _TECHMAP_CONSTVAL_A_ = 0;
 
     if(_TECHMAP_CONSTMSK_A_ == 1) begin
-        D_BUFF #(.DSEL(_TECHMAP_CONSTVAL_A_)) _TECHMAP_REPLACE_ (.Q(Q));
+        d_buff #(.DSEL(_TECHMAP_CONSTVAL_A_)) _TECHMAP_REPLACE_ (.Q(Q));
     end
     else begin
-        OUT_BUFF _TECHMAP_REPLACE_ (.Q(Q), .A(A));
+        out_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
     end
 endmodule
 
@@ -17,6 +17,6 @@ module \$__in_buff (Q, A);
 	output Q;
     input A;
 
-    IN_BUFF _TECHMAP_REPLACE_ (.Q(Q), .A(A));
+    in_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
 
 endmodule
